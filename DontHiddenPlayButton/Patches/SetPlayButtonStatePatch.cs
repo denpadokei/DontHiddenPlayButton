ef277fdc9195daf63e50710495601e6434391f26
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-04-21 22:51:10 +0900

    プロジェクト ファイルを追加します。

---
+++ DontHiddenPlayButton/Patches/SetPlayButtonStatePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using IPA.Utilities;
+using ScoreSaber.UI.Other;
+using System;
+using UnityEngine.UI;
+
+namespace DontHiddenPlayButton.Patches
+{
+    [HarmonyPatch(typeof(ScoreSaberLeaderboardView),
+        nameof(ScoreSaberLeaderboardView.SetPlayButtonState),
+        new Type[] { typeof(bool) })]
+    public class SetPlayButtonStatePatch
+    {
+        public static bool Prefix(ref bool state, ScoreSaberLeaderboardView __instance)
+        {
+            var button = __instance.GetField<Button, ScoreSaberLeaderboardView>("#=zMZIPr5eSY$UN");
+            button.interactable = state;
+            return false;
+        }
+    }
+}
---
+++ DontHiddenPlayButton/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace DontHiddenPlayButton
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        public const string HARMONY_ID = "DontHiddenPlayButton.denpadokei.com.github";
+        private Harmony harmony;
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("DontHiddenPlayButton initialized.");
+            this.harmony = new Harmony(HARMONY_ID);
+        }
+        [OnStart]
+        public void OnApplicationStart() => Log.Debug("OnApplicationStart");
+
+        [OnExit]
+        public void OnApplicationQuit() => Log.Debug("OnApplicationQuit");
+
+        [OnEnable]
+        public void OnEnable() => this.harmony.PatchAll(Assembly.GetExecutingAssembly());
+        [OnDisable]
+        public void OnDisable() => this.harmony.UnpatchAll(HARMONY_ID);
+    }
+}
