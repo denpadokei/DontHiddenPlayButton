007acabda2f41a576ef86bc13d0ddc8e32d924cb
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-06-02 18:42:19 +0900

    fix 1.16.1

--- DontHiddenPlayButton/Patches/SetPlayButtonStatePatch.cs
+++ DontHiddenPlayButton/Patches/SetPlayButtonStatePatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using BS_Utils.Utilities;
+using HarmonyLib;
 using IPA.Utilities;
 using ScoreSaber.UI.Other;
 using System;
@@ -11,11 +12,28 @@ namespace DontHiddenPlayButton.Patches
         new Type[] { typeof(bool) })]
     public class SetPlayButtonStatePatch
     {
+        private static bool _isWIP = false;
+
         public static bool Prefix(ref bool state, ScoreSaberLeaderboardView __instance)
         {
-            var button = __instance.GetField<Button, ScoreSaberLeaderboardView>("#=zMZIPr5eSY$UN");
-            button.interactable = state;
+            var button = __instance.GetField<Button, ScoreSaberLeaderboardView>("_playButton");
+            if (_isWIP) {
+                button.interactable = false;
+            }
+            else {
+                button.interactable = state;
+            }
             return false;
+        }
+
+        static SetPlayButtonStatePatch()
+        {
+            BSEvents.levelSelected += BSEvents_levelSelected;
+        }
+
+        private static void BSEvents_levelSelected(LevelCollectionViewController arg1, IPreviewBeatmapLevel arg2)
+        {
+            _isWIP = arg2.levelID.EndsWith(" WIP");
         }
     }
 }
